b138aec4f7e61b78d416953e9c1c053f7cd54dfb
Oskar-Maria Seeberger <110058070+hyepr@users.noreply.github.com>
2026-07-06 19:51:40 +0000

    Added Payload CMS!

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -8,4 +9,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPayload(nextConfig);
